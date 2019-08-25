c0c63ceceab6138ddcc15d40c3df839fb1c61fa3
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-08-25 22:05:33 +0900

    Add set_ccr_register function

--- stm32_antiphase_pwm.cpp
+++ stm32_antiphase_pwm.cpp
@@ -8,6 +8,8 @@
 
 #include "stm32_antiphase_pwm.hpp"
 
+static void set_ccr_register(TIM_HandleTypeDef *htim, uint32_t channel,uint32_t pulse);
+
 Stm32AntiphasePwm::Stm32AntiphasePwm(TIM_HandleTypeDef *htim, uint32_t channel_a, uint32_t channel_b) {
     this->htim = htim;
 
@@ -39,4 +41,31 @@ int Stm32AntiphasePwm::update_duty(double duty_rate){
     htim->Instance->CCR2 = PWM_DUTY_ZERO - difference;
 
     return retval;
+}
+
+void set_ccr_register(TIM_HandleTypeDef *htim, uint32_t channel,uint32_t pulse)
+{
+    switch (channel)
+    {
+    case TIM_CHANNEL_1:
+        htim->Instance->CCR1 = pulse;
+        break;
+
+    case TIM_CHANNEL_2:
+        htim->Instance->CCR2 = pulse;
+        break;
+
+    case TIM_CHANNEL_3:
+        htim->Instance->CCR3 = pulse;
+        break;
+
+    case TIM_CHANNEL_4:
+        htim->Instance->CCR4 = pulse;
+        break;
+
+    default:
+        break;
+    }
+
+    return;
 }
